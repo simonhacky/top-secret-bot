5bb240b360088d34dde03672cce94830f371e0c8
Simon <hackners@cip.ifi.lmu.de>
2021-03-04 16:38:34 +0100

    added tic tac toe game

---
+++ src/TicTacToe.py
@@ -0,0 +1,61 @@
+class Player:
+    def __init__(self, player_id, alliance):
+        self.player_id = player_id
+        self.alliance = alliance
+
+
+class Board:
+    fields = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+
+    def move(self, index, player):
+        self.fields[index] = player.alliance
+
+    def is_won_by(self, player):
+        possible_combinations = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+        for combination in possible_combinations:
+            if all(self.fields[i] == player.alliance for i in combination):
+                return True
+        return False
+
+    def is_full(self):
+        return all(i != ' ' for i in self.fields)
+
+    def to_string(self):
+        string = ''
+        for i in range(len(self.fields)):
+            if i % 3 == 0 and i != 0:
+                string += '\n'
+            if self.fields[i] == 'o':
+                string += '<:x:1451>'
+            if self.fields[i] == 'x':
+                string += '<:o:1447>'
+            if self.fields[i] == ' ':
+                string += '<:white_square_button:1546>'
+        return string
+
+
+class Game:
+    def __init__(self, player_x_id, player_o_id):
+        self.player_x = Player(player_x_id, 'x')
+        self.player_o = Player(player_o_id, 'o')
+        self.board = Board()
+
+    def move(self, player_id, index):
+        self.board.move(index - 1, self.player_x if player_id == self.player_x.player_id else self.player_o)
+        return self.board.to_string()
+
+    def get_winner(self):
+        if self.board.is_won_by(self.player_x):
+            return 'Player X has won the game!'
+        if self.board.is_won_by(self.player_o):
+            return 'Player O has won the game!'
+        if self.board.is_full():
+            return 'Game is drawn!'
+        return False
+
+    def reset(self):
+        self.board.fields = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+        return self.board.to_string()
+
+
+game = None
--- src/__init__.py
+++ src/__init__.py
@@ -1,29 +1,71 @@
 import discord
+import TicTacToe
 
 
 class Bot(discord.Client):
-
     prefix = '%'
 
     async def on_ready(self):
         print("connected to server: ", self.guilds.__getitem__(0))
 
     async def on_message(self, message):
-        if message.author.id == self.user.id:
-            return
-
-        elif message.content.startswith(Bot.prefix):
+        if message.author.id != self.user.id and message.content.startswith(Bot.prefix):
             message.content = message.content[1:]
             if message.content.startswith('prefix'):
-                Bot.prefix = message.content[(len(message.content)-1)]
+                Bot.prefix = message.content[(len(message.content) - 1)]
                 strmsg = "New prefix: " + Bot.prefix
                 await message.channel.send(strmsg)
 
-        else:
-            return
-        """if message.author.id != self.user.id:
-            await message.channel.send("du bist behindert")"""
+            if message.content.startswith('play'):
+                await play(message)
+
+            if message.content.startswith('tictactoe'):
+                await tictactoe(message)
+
+            if message.content.startswith('join'):
+                await join(message)
+
+            if message.content.startswith('cross'):
+                await cross(message)
+
+            if message.content.startswith('reset'):
+                await reset(message)
+
+
+tic_tac_toe = TicTacToe
+player_ids = []
+
+
+async def play(message):
+    await message.channel.send(
+        'You can play these games: \n    -tictactoe\n\nTo play one, simply type its name after a "%" prefix!')
+
+
+async def tictactoe(message):
+    player_ids.append(message.author.id)
+    await message.channel.send('You are Player X!\nYour mate can join via "%join"!')
+
+
+async def join(message):
+    player_ids.append(message.author.id)
+    await message.channel.send(
+        'You are Player O! Player X can start now with "%cross x" where x denotes a number between 1 and 9')
+    tic_tac_toe.game = tic_tac_toe.Game(player_ids[0], player_ids[1])
+
+
+async def cross(message):
+    message.content = message.content[6:]
+    await message.channel.send(tic_tac_toe.game.move(message.author.id, int(message.content)))
+    if tic_tac_toe.game.get_winner():
+        await message.channel.send(tic_tac_toe.game.get_winner())
+
+
+async def reset(message):
+    if not tic_tac_toe.game.get_winner():
+        await message.channel.send('There is already a game in progress!')
+    else:
+        await message.channel.send(tic_tac_toe.game.reset())
 
 
 client: Bot = Bot()
-client.run('ODE2Mzc4MzkyOTY5NjA5MjU3.YD6FoA.DCtvB90xmQ09qPv7_bPa8tbEdaY')
+client.run('ODE2Mzc4MzkyOTY5NjA5MjU3.YD6FoA.iE-wjYBmPZOg2-LUiaJ9QMAfTA0')
